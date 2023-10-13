82279eea58e289843fe3fc33c65a1a8529c6e2cf
Joey Fitzpatrick <joeyf116@yahoo.com>
2023-10-13 16:13:31 -0400

    Add chat stream handling

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,15 +6,15 @@ import useAutoResizeTextArea from "@/hooks/useAutoResizeTextArea";
 import Message from "./Message";
 import { DEFAULT_OPENAI_MODEL } from "@/shared/Constants";
 import Image from "next/image";
+import { useChat } from "ai/react";
 
 const Chat = (props: any) => {
 	const { toggleComponentVisibility } = props;
 
-	const [isLoading, setIsLoading] = useState(false);
+	const { input, handleInputChange, handleSubmit, isLoading, messages } =
+		useChat();
+
 	const [errorMessage, setErrorMessage] = useState("");
-	const [showEmptyChat, setShowEmptyChat] = useState(true);
-	const [conversation, setConversation] = useState<any[]>([]);
-	const [message, setMessage] = useState("");
 	const textAreaRef = useAutoResizeTextArea();
 	const bottomOfChatRef = useRef<HTMLDivElement>(null);
 
@@ -25,80 +25,13 @@ const Chat = (props: any) => {
 			textAreaRef.current.style.height = "24px";
 			textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
 		}
-	}, [message, textAreaRef]);
+	}, [input, textAreaRef]);
 
 	useEffect(() => {
 		if (bottomOfChatRef.current) {
 			bottomOfChatRef.current.scrollIntoView({ behavior: "smooth" });
 		}
-	}, [conversation]);
-
-	const sendMessage = async (e: any) => {
-		e.preventDefault();
-
-		// Don't send empty messages
-		if (message.length < 1) {
-			setErrorMessage("Please enter a message.");
-			return;
-		} else {
-			setErrorMessage("");
-		}
-
-		setIsLoading(true);
-
-		// Add the message to the conversation
-		setConversation([
-			...conversation,
-			{ content: message, role: "user" },
-			{ content: null, role: "system" },
-		]);
-
-		// Clear the message & remove empty chat
-		setMessage("");
-		setShowEmptyChat(false);
-
-		try {
-			const response = await fetch(`/api/openai`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					messages: [...conversation, { content: message, role: "user" }],
-					model: selectedModel,
-				}),
-			});
-
-			if (response.ok) {
-				const data = await response.json();
-
-				// Add the message to the conversation
-				setConversation([
-					...conversation,
-					{ content: message, role: "user" },
-					{ content: data.message, role: "system" },
-				]);
-			} else {
-				console.error(response);
-				setErrorMessage(response.statusText);
-			}
-
-			setIsLoading(false);
-		} catch (error: any) {
-			console.error(error);
-			setErrorMessage(error.message);
-
-			setIsLoading(false);
-		}
-	};
-
-	const handleKeypress = (e: any) => {
-		// It's triggers by pressing the enter key
-		if (e.keyCode == 13 && !e.shiftKey) {
-			sendMessage(e);
-			e.preventDefault();
-		}
-	};
+	}, [messages]);
 
 	return (
 		<div className="flex max-w-full flex-1 flex-col">
@@ -120,29 +53,28 @@ const Chat = (props: any) => {
 				<div className="flex-1 overflow-hidden">
 					<div className="react-scroll-to-bottom--css-ikyem-79elbk h-full dark:bg-gray-800">
 						<div className="react-scroll-to-bottom--css-ikyem-1n7m0yu">
-							{!showEmptyChat && conversation.length > 0 ? (
+							{messages.length > 0 ? (
 								<div className="flex flex-col items-center text-sm bg-gray-800">
 									<div className="flex w-full items-center justify-center gap-1 border-b border-black/10 bg-gray-50 p-3 text-gray-500 dark:border-gray-900/50 dark:bg-gray-700 dark:text-gray-300">
 										Model: {selectedModel.name}
 									</div>
-									{conversation.map((message, index) => (
+									{messages.map((message, index) => (
 										<Message key={index} message={message} />
 									))}
 									<div className="w-full h-32 md:h-48 flex-shrink-0"></div>
 									<div ref={bottomOfChatRef}></div>
 								</div>
 							) : null}
-							{showEmptyChat ? (
+							{messages.length === 0 ? (
 								<div className="py-10 relative w-full flex flex-col h-full">
-									<div className="flex items-center justify-center gap-2">
-									</div>
-									<div className="flex flex-col h-screen justify-center space-y-2 align-middle items-center">
+									<div className="flex items-center justify-center gap-2"></div>
+									<div className="flex flex-col h-[100dvh] justify-center space-y-2 align-middle items-center">
 										<Image
 											src="/MultiColorS_48.png"
 											width={100}
 											height={100}
 											alt="Picture of the author"
-                      className="shadow-full"
+											className="shadow-full"
 										/>
 										<h1 className="text-2xl sm:text-4xl font-semibold text-gray-200 dark:text-gray-600">
 											SES | GPT
@@ -155,7 +87,10 @@ const Chat = (props: any) => {
 					</div>
 				</div>
 				<div className="absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
-					<form className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+					<form
+						className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl"
+						onSubmit={handleSubmit}
+					>
 						<div className="relative flex flex-col h-full flex-1 items-stretch md:flex-col">
 							{errorMessage ? (
 								<div className="mb-2 md:mb-0">
@@ -167,8 +102,7 @@ const Chat = (props: any) => {
 							<div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
 								<textarea
 									ref={textAreaRef}
-									value={message}
-									tabIndex={0}
+									value={input}
 									data-id="root"
 									style={{
 										height: "24px",
@@ -178,12 +112,12 @@ const Chat = (props: any) => {
 									// rows={1}
 									placeholder="Send a message..."
 									className="m-0 w-full resize-none border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent pl-2 md:pl-0"
-									onChange={(e) => setMessage(e.target.value)}
-									onKeyDown={handleKeypress}
+									onChange={handleInputChange}
 								></textarea>
 								<button
-									disabled={isLoading || message?.length === 0}
-									onClick={sendMessage}
+									type="submit"
+									tabIndex={0}
+									disabled={isLoading || input?.length === 0}
 									className="absolute p-1 rounded-md bottom-1.5 md:bottom-2.5 bg-transparent disabled:bg-gray-500 right-1 md:right-2 disabled:opacity-40"
 								>
 									<FiSend className="h-4 w-4 mr-1 text-white " />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,23 @@ import MobileSiderbar from "@/components/MobileSidebar";
 import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
-  const [isComponentVisible, setIsComponentVisible] = useState(false);
+	const [isComponentVisible, setIsComponentVisible] = useState(false);
 
-  const toggleComponentVisibility = () => {
-    setIsComponentVisible(!isComponentVisible);
-  };
+	const toggleComponentVisibility = () => {
+		setIsComponentVisible(!isComponentVisible);
+	};
 
-  return (
-    <main className="overflow-hidden w-full h-screen relative flex">
-      {isComponentVisible ? (
-        <MobileSiderbar toggleComponentVisibility={toggleComponentVisibility} />
-      ) : null}
-      <div className="dark hidden flex-shrink-0 bg-gray-900 md:flex md:w-[260px] md:flex-col">
-        <div className="flex h-full min-h-0 flex-col">
-          <Sidebar />
-        </div>
-      </div>
-      <Chat toggleComponentVisibility={toggleComponentVisibility} />
-    </main>
-  );
+	return (
+		<main className="overflow-hidden w-full h-[100dvh] relative flex">
+			{isComponentVisible ? (
+				<MobileSiderbar toggleComponentVisibility={toggleComponentVisibility} />
+			) : null}
+			<div className="dark hidden flex-shrink-0 bg-gray-900 md:flex md:w-[260px] md:flex-col">
+				<div className="flex h-full min-h-0 flex-col">
+					<Sidebar />
+				</div>
+			</div>
+			<Chat />
+		</main>
+	);
 }
